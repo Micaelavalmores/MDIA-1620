afb220bcf3a37e67530a803bd2def82fd9a84551
Micaela Valmores <micaelavalmores@Micaelas-MacBook-Air.local>
2024-09-20 15:34:07 -0700

    initialize stables and add stables

---
+++ stables.js
@@ -0,0 +1,15 @@
+let horseName = "Sparkles";
+console.log(horseName);
+
+horseName = "Yeehaw"
+console.log(horseName)
+
+
+let horseAge = 2;
+console.log(horseAge)
+
+let isHorseInside = true;
+console.log(isHorseInside)
+
+
+
