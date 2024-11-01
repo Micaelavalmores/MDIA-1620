8d6bda6bef7fe4d9ed3aa08e721128c2eef78b45
Micaela Valmores <micaelavalmores@Micaelas-MacBook-Air.local>
2024-10-31 21:35:17 -0700

    more practice

--- Practice.js
+++ Practice.js
@@ -37,18 +37,18 @@ let apartmentLocation = "Downtown Vancouver"
 
 //console.log(girlNames);
 
-console.log(`${girlName} has a boyfriend named ${boyfriendName} and he had an affair with ${girlNames.at(1)}`);
-console.log(`${girlName} was very ${relationshipStatuses.at(1)} because she got ${relationshipStatuses.at(2)} on by her boyfriend`);
-console.log(`Once ${girlName} turned ${girlAge}, she started ${isGirlrRaving} and met ${otherMan}`);
-console.log(`However, ${otherMan} has a girlfriend named ${girlNames.at(2)}`);
-if (girlName === "Vivian") {
-    console.log(`${otherMan} hooked up with ${girlName} that night without ${girlNames.at(2)} knowing`)
-} else {
-    console.log(`${otherMan} did not entertain ${girlNAme} during that night`)
-};
-console.log(`${girlNames.at(2)} was devastated and ${relationshipStatuses.at(1)} and broke up with ${otherMan}`);
-console.log(`Soon after, ${girlName} moved in ${otherMan}'s studio apartment at ${apartmentLocation} with a rent costing ${aptRent}`);
-console.log(`It took them ${word.length} years of living together until ${girlName} started cheating on ${otherMan} after she had gone ${isGirlrRaving}`);
+// console.log(`${girlName} has a boyfriend named ${boyfriendName} and he had an affair with ${girlNames.at(1)}`);
+// console.log(`${girlName} was very ${relationshipStatuses.at(1)} because she got ${relationshipStatuses.at(2)} on by her boyfriend`);
+// console.log(`Once ${girlName} turned ${girlAge}, she started ${isGirlrRaving} and met ${otherMan}`);
+// console.log(`However, ${otherMan} has a girlfriend named ${girlNames.at(2)}`);
+// if (girlName === "Vivian") {
+//     console.log(`${otherMan} hooked up with ${girlName} that night without ${girlNames.at(2)} knowing`)
+// } else {
+//     console.log(`${otherMan} did not entertain ${girlNAme} during that night`)
+// };
+// console.log(`${girlNames.at(2)} was devastated and ${relationshipStatuses.at(1)} and broke up with ${otherMan}`);
+// console.log(`Soon after, ${girlName} moved in ${otherMan}'s studio apartment at ${apartmentLocation} with a rent costing ${aptRent}`);
+// console.log(`It took them ${word.length} years of living together until ${girlName} started cheating on ${otherMan} after she had gone ${isGirlrRaving}`);
 
 //lessonn 8 Practice
 function square(number) {
@@ -56,14 +56,45 @@ function square(number) {
 }
 square(5);
 
-console.log(girlName + " is " + " an " + girlNickname);
- let templateLiteralIntro = `The girl's name is ${girlName} and she likes ${isGirlrRaving}!`
- console.log(templateLiteralIntro);
+// console.log(girlName + " is " + " an " + girlNickname);
+//  let templateLiteralIntro = `The girl's name is ${girlName} and she likes ${isGirlrRaving}!`
+//  console.log(templateLiteralIntro);
 
- function isSheCheating(girl) {
-  if (isGirlrRaving[girl]) {
-    return girlNames[girl] + " is not cheating";
-  }
-    return girlNames[girl] + " is cheating";
- }
- console.log(isSheCheating(1));
+//  function isSheCheating(girl) {
+//   if (isGirlrRaving[girl]) {
+//     return girlNames[girl] + " is not cheating";
+//   }
+//     return girlNames[girl] + " is cheating";
+//  }
+//  console.log(isSheCheating(1));
+
+
+function square(number) {
+  return number + number;
+}
+square(5);
+console.log(square(5));
+
+function whatsCheating(answer) {
+  return "She is " + answer;
+}
+whatsCheating("unloyal");
+console.log(whatsCheating("unloyal"));
+
+
+function paper(width, length) {
+  return width * length;
+}
+
+console.log(paper(3, 9));
+
+function sephora(lipMask, perfume, blush) {
+  if (sephora === "foundation"); {
+  return lipMask + " is the best lipmask, " + perfume + " has the best scents, and " + blush + " has the best blush colors";
+} 
+   return "Sephora has boring brands";
+}
+
+  console.log(sephora("Laneige", "Replica", "Rare Beauty"));
+
+
